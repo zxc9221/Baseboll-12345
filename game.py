74a074213b69de6d1982f28ba6faa20da60d2e2e
hyewonkim <hyewonds.kim@samsung.com>
2025-07-31 14:39:19 +0900

    move GameResult

--- game.py
+++ game.py
@@ -1,21 +1,7 @@
 from math import gamma
-class GameResult:
-    def __init__(self, solved, strikes, balls):
-        self._solved = solved
-        self._strikes = strikes
-        self._balls = balls
 
-    @property
-    def solved(self):
-        return self._solved
+from game_result import GameResult
 
-    @property
-    def strikes(self):
-        return self._strikes
-
-    @property
-    def balls(self):
-        return self._balls
 
 class Game:
     def __init__(self):
@@ -32,7 +18,7 @@ class Game:
     def guess(self, guess_number):
         self._assert_illegal_value(guess_number)
         if guess_number == self._question:
-            return  GameResult(True, 3, 0)
+            return GameResult(True, 3, 0)
         else:
             return GameResult(False, 0, 0)
 
@@ -42,7 +28,7 @@ class Game:
         if len(guess_number) != 3:
             raise TypeError("입력은 3 자리 문자열이여야 합니다.")
         if not guess_number.isdigit():
-                raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
+            raise TypeError("모든 문자는 숫자로 구성되어야 합니다.")
         if self._isDuplicatedNumber(guess_number):
             raise TypeError("중복된 숫자가 존재합니다.")
 
--- test_game.py
+++ test_game.py
@@ -1,4 +1,5 @@
 import pytest
+
 from game import Game, GameResult
 
 
@@ -6,13 +7,16 @@ from game import Game, GameResult
 def game():
     return Game()
 
+
 def assert_illegal_argument(game, guess_number):
     with pytest.raises(TypeError):
         game.guess(guess_number)
 
+
 @pytest.mark.parametrize("invalid_input", [None, "12", "1234", "12s", "121"])
 def test_exception_when_input(game, invalid_input):
     assert_illegal_argument(game, invalid_input)
+
 
 def assert_matched_number(result, solved, strikes, balls):
     assert result is not None
@@ -20,9 +24,11 @@ def assert_matched_number(result, solved, strikes, balls):
     assert result.strikes == strikes
     assert result.balls == balls
 
+
 def test_return_solved_result_if_matched_number(game):
     game.question = "123"
     assert_matched_number(game.guess("123"), solved=True, strikes=3, balls=0)
+
 
 def test_return_solved_result_if_unmatched_number(game):
     game.question = "123"
